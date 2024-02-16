d8c2a60ddd2fa19e63c7d1e8981e40335be30afd
Masaaki Hijikata <hijimasa@gmail.com>
2024-02-17 07:12:52 +0900

    [fix] ステージをURDFインポートだとCollisionが大雑把過ぎて使えな いので、直接USDを扱うように修正 [fix] velocity_pubが横方向の速度を出力しない不具合を修正 [add] 生成するURDFモデルの位置を固定できるオプションを追加 [bug] 生成するURDFモデルの位置を固定にすると、ステージの原点から動かない...

--- colcon_ws/src/sample_robot_sim/launch/sample_robot_spawn_for_core.launch.py
+++ colcon_ws/src/sample_robot_sim/launch/sample_robot_spawn_for_core.launch.py
@@ -1,5 +1,6 @@
 import os
 import math
+import pathlib
 
 from ament_index_python.packages import get_package_share_directory
 
@@ -9,35 +10,18 @@ from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchD
 from launch.actions import RegisterEventHandler
 from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
 import xacro
 
-ROBOT1_START_POSITION    = [1.0, 2.5, 0.05]
+ROBOT1_START_POSITION    = [3.0, 5.5, 1.05]
 ROBOT1_START_YAW = 0.0
 def generate_launch_description():
     # Launch Arguments
     use_sim_time = LaunchConfiguration('use_sim_time', default=True)
-
-    gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('ros_gz_sim'), 'launch'), '/gz_sim.launch.py']),
-                launch_arguments=[('gz_args', [' -r -v 4 empty.sdf'])]
-             )
-
-    core_stage_description_path = os.path.join(
-        get_package_share_directory('core_stage_description'))
-
-    core_stage_xacro_file = os.path.join(core_stage_description_path,
-                              'urdf',
-                              'core_stage.urdf.xacro')
-    # xacroをロード
-    core_stage_doc = xacro.process_file(core_stage_xacro_file, mappings={'use_sim' : 'true'})
-    # xacroを展開してURDFを生成
-    core_stage_desc = core_stage_doc.toprettyxml(indent='  ')
 
     flying_disc_description_path = os.path.join(
         get_package_share_directory('flying_disc_description'))
@@ -45,24 +29,33 @@ def generate_launch_description():
     flying_disc_xacro_file = os.path.join(flying_disc_description_path,
                               'urdf',
                               'flying_disc.urdf.xacro')
-
+    flying_disc_urdf_path = os.path.join(flying_disc_description_path, 'urdf', 'flying_disc.urdf')
     # xacroをロード
     flying_disc_doc = xacro.process_file(flying_disc_xacro_file, mappings={'use_sim' : 'true'})
     # xacroを展開してURDFを生成
     flying_disc_desc = flying_disc_doc.toprettyxml(indent='  ')
+    f = open(flying_disc_urdf_path, 'w')
+    f.write(flying_disc_desc)
+    f.close()
+    flying_disc_relative_urdf_path = pathlib.Path(flying_disc_urdf_path).relative_to(os.getcwd())
+
 
     sample_robot_description_path = os.path.join(
         get_package_share_directory('sample_robot_description'))
 
-    xacro_file = os.path.join(sample_robot_description_path,
+    sample_robot_xacro_file = os.path.join(sample_robot_description_path,
                               'robots',
                               'sample_robot.urdf.xacro')
+    sample_robot_urdf_path = os.path.join(sample_robot_description_path, 'urdf', 'sample_robot.urdf')
     # xacroをロード
-    doc = xacro.process_file(xacro_file, mappings={'use_sim' : 'true'})
+    sample_robot_doc = xacro.process_file(sample_robot_xacro_file, mappings={'use_sim' : 'true'})
     # xacroを展開してURDFを生成
-    robot_desc = doc.toprettyxml(indent='  ')
-
-    params = {'robot_description': robot_desc}
+    sample_robot_desc = sample_robot_doc.toprettyxml(indent='  ')
+    f = open(sample_robot_urdf_path, 'w')
+    f.write(sample_robot_desc)
+    f.close()
+    sample_robot_relative_urdf_path = pathlib.Path(sample_robot_urdf_path).relative_to(os.getcwd())
+    params = {'robot_description': sample_robot_desc}
 
     rviz_config_file = os.path.join(sample_robot_description_path, 'config', 'sample_robot_description.rviz')
     
@@ -73,34 +66,30 @@ def generate_launch_description():
         parameters=[params]
     )
 
-    gz_spawn_entity = Node(
-        package='ros_gz_sim',
-        executable='create',
+    isaac_spawn_robot = Node(
+        package="isaac_ros2_scripts",
+        executable="spawn_robot",
+        parameters=[{'urdf_path': str(sample_robot_relative_urdf_path),
+                    'x' : ROBOT1_START_POSITION[0],
+                    'y' : ROBOT1_START_POSITION[1],
+                    'z' : ROBOT1_START_POSITION[2],
+                    'R' : 0.0,
+                    'P' : 0.0,
+                    'Y' : ROBOT1_START_YAW,
+                    }],
         output='screen',
-        arguments=['-string', robot_desc,
-                   '-name', 'sample_robot',
-                   '-x', str(ROBOT1_START_POSITION[0]),
-                   '-y', str(ROBOT1_START_POSITION[1]),
-                   '-z', str(ROBOT1_START_POSITION[2]),
-                   '-R', str(0.0),
-                   '-P', str(0.0),
-                   '-Y', str(ROBOT1_START_YAW),
-                   '-allow_renaming', 'false'],
     )
 
-    core_stage_gz_spawn_entity = Node(
-        package='ros_gz_sim',
-        executable='create',
-        output='screen',
-        arguments=['-string', core_stage_desc,
-                   '-name', 'core_stage',
-                   '-x', str(0.0),
-                   '-z', LaunchConfiguration('sensor_coord_Z', default=0.05),
-                   '-y', LaunchConfiguration('sensor_coord_Y', default=0.0),
-                   '-R', LaunchConfiguration('sensor_orien_R', default=0.0),
-                   '-P', LaunchConfiguration('sensor_orien_P', default=0.0),
-                   '-Y', LaunchConfiguration('sensor_orien_Y', default=0.0),
-                   '-allow_renaming', 'true'],
+    isaac_prepare_sensors = Node(
+        package="isaac_ros2_scripts",
+        executable="prepare_sensors",
+        parameters=[{'urdf_path': str(sample_robot_relative_urdf_path)}],
+    )
+
+    isaac_prepare_robot_controller = Node(
+        package="isaac_ros2_scripts",
+        executable="prepare_robot_controller",
+        parameters=[{'urdf_path': str(sample_robot_relative_urdf_path)}],
     )
 
     flying_disc_gz_spawn_entity_list = []
@@ -109,58 +98,60 @@ def generate_launch_description():
         disc_z = 0.2024*math.sin(-0.47473 + (index+1) * math.pi / 10)
         flying_disc_gz_spawn_entity_list.append(
             Node(
-                package='ros_gz_sim',
-                executable='create',
+                package="isaac_ros2_scripts",
+                executable="spawn_robot",
+                parameters=[{'urdf_path': str(flying_disc_relative_urdf_path),
+                            'x' : ROBOT1_START_POSITION[0]+disc_x*math.cos(ROBOT1_START_YAW),
+                            'y' : ROBOT1_START_POSITION[1]+disc_x*math.sin(ROBOT1_START_YAW),
+                            'z' : ROBOT1_START_POSITION[2] + 0.4425+disc_z,
+                            'R' : 0.0,
+                            'P' : -(index+1) * math.pi / 10,
+                            'Y' : ROBOT1_START_YAW,
+                            }],
                 output='screen',
-                arguments=['-string', flying_disc_desc,
-                    '-name', 'disc' + str(index),
-                    '-x', str(ROBOT1_START_POSITION[0]+disc_x*math.cos(ROBOT1_START_YAW)),
-                    '-y', str(ROBOT1_START_POSITION[1]+disc_x*math.sin(ROBOT1_START_YAW)),
-                    '-z', str(ROBOT1_START_POSITION[2] + 0.4425+disc_z),
-                    '-R', str(0.0),
-                    '-P', str(-(index+1) * math.pi / 10),
-                    '-Y', str(ROBOT1_START_YAW),
-                    '-allow_renaming', 'true'],
-            )
+                )
         )
 
-    load_joint_state_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'joint_state_broadcaster'],
-        output='screen'
+    robot_controllers = PathJoinSubstitution(
+        [
+            FindPackageShare("sample_robot_description"),
+            "config",
+            "sample_robot_config.yaml",
+        ]
+    )
+    control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[params, robot_controllers],
+        output={
+            "stdout": "screen",
+            "stderr": "screen",
+        },
+    )
+    
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+    )
+    
+    omni_wheel_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["omni_wheel_controller", "--controller-manager", "/controller_manager"],
     )
 
-    load_omni_wheel_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'omni_wheel_controller'],
-        output='screen'
-    )
+    #load_joint_trajectory_controller = ExecuteProcess(
+    #    cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+    #         'joint_trajectory_controller'],
+    #    output='screen'
+    #)
 
-    load_joint_trajectory_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'joint_trajectory_controller'],
-        output='screen'
-    )
-
-    load_velocity_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'velocity_controller'],
-        output='screen'
-    )
-
-    # Bridge
-    bridge = Node(
-        package='ros_gz_bridge',
-        executable='parameter_bridge',
-        arguments=['/front_left/scan@sensor_msgs/msg/LaserScan@ignition.msgs.LaserScan',
-                   '/rear_right/scan@sensor_msgs/msg/LaserScan@ignition.msgs.LaserScan',
-                   '/camera_info@sensor_msgs/msg/CameraInfo@ignition.msgs.CameraInfo',
-                   '/image_raw@sensor_msgs/msg/Image@ignition.msgs.Image',
-                   '/depth_camera/camera_info@sensor_msgs/msg/CameraInfo@ignition.msgs.CameraInfo',
-                   '/depth_camera/image_raw@sensor_msgs/msg/Image@ignition.msgs.Image',
-                   '/depth_camera/image_raw/points@sensor_msgs/msg/PointCloud2@ignition.msgs.PointCloudPacked'],
-        output='screen'
-    )
+    #load_velocity_controller = ExecuteProcess(
+    #    cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+    #         'velocity_controller'],
+    #    output='screen'
+    #)
 
     velocity_converter = Node(
         package='velocity_pub',
@@ -182,39 +173,22 @@ def generate_launch_description():
     return LaunchDescription([
         RegisterEventHandler(
             event_handler=OnProcessExit(
-                target_action=gz_spawn_entity,
-                on_exit=[core_stage_gz_spawn_entity],
+                target_action=isaac_spawn_robot,
+                on_exit=[isaac_prepare_sensors],
             )
         ),
         RegisterEventHandler(
             event_handler=OnProcessExit(
-                target_action=gz_spawn_entity,
-                on_exit=[load_joint_state_controller],
+                target_action=isaac_prepare_sensors,
+                on_exit=[isaac_prepare_robot_controller],
             )
         ),
-        RegisterEventHandler(
-            event_handler=OnProcessExit(
-               target_action=load_joint_state_controller,
-               on_exit=[load_omni_wheel_controller],
-            )
-        ),
-        RegisterEventHandler(
-            event_handler=OnProcessExit(
-               target_action=load_omni_wheel_controller,
-               on_exit=[load_joint_trajectory_controller],
-            )
-        ),
-        RegisterEventHandler(
-            event_handler=OnProcessExit(
-               target_action=load_joint_trajectory_controller,
-               on_exit=[load_velocity_controller],
-            )
-        ),
-        gazebo,
         node_robot_state_publisher,
-        gz_spawn_entity,
-        bridge,
+        isaac_spawn_robot,
+        control_node,
+        joint_state_broadcaster_spawner,
+        omni_wheel_controller_spawner,
         velocity_converter,
         rviz,
-    ] + flying_disc_gz_spawn_entity_list
+    ] #+ flying_disc_gz_spawn_entity_list
     )
